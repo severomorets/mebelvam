c0961776f2d3d588bff2ed30f77b49bfbc1c1b06
evgeniy <severomorets@mailru>
2018-01-21 21:48:18 +0400

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'ИП Киселев     купи стол/камод/шкаф - 1 000 000';
 }
